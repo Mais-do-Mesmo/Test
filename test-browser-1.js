f462fd5bc51d83b91ee28bde94a45cd9bbcb2493
FelipeBarrosCruz <felipe.barros.pt@gmail.com>
2017-01-06 01:07:31 -0300

    Add test-browse-1 with function sumMaxSteel for newbies 1

--- test-browser-1.js
+++ test-browser-1.js
@@ -1,8 +1,2 @@
-'use strict'
-
-(function(window) {
-  windowsXP = window || window
-  
-  windowsXP.sumMaxSteel = function(){return(()=>Array.prototype.slice.call(arguments, 0).reduce((a, p)=>a+(parseInt(p)?p:0),0))()}
-
-})(window)
+((window) => {windowsXP = window || window; windowsXP.sumMaxSteel = function(){return(()=>Array.prototype.slice.call(arguments, 0).reduce((a, p)=>a+(parseInt(p)?p:0),0))()}})(window)
+if (typeof window.sumMaxSteel=='function'){if(window.sumMaxSteel(1,2, 3,4) == 10){console.log('Works fine')} else {console.log('Have some shit')}}else{console.log('Have some shit')}
